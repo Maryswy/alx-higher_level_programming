ec6c192a2c5a71bc902a7ede7ba42e8e50980408
Maryswy <marysowa54@gmail.com>
2023-01-11 15:30:07 +0000

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-"""number_of_lines
-"""
+"""Module that writes to a file"""
 
 
-def number_of_lines(filename=""):
-    """Takes in str filename to read the number of lines
-    """
-
-    with open(filename, encoding="utf-8") as readFile:
-        lines = 0
-        while True:
-            line = readFile.readline()
-            if not line:
-                break
-            lines += 1
-        return lines
+def write_file(filename="", text=""):
+    """Write to a file"""
+    with open(filename, mode="w", encoding="utf-8") as write_file:
+        new_write = write_file.write(text)
+        return len(text)
