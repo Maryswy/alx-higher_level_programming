4b12285148c2e3f524473dc5b33cf8cc6def9f41
Maryswy <marysowa54@gmail.com>
2023-04-26 18:30:23 +0300

    A script that gets the contents of a webpage and stores it in a file (Loripsum)

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,26 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+
+// Get command line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make request to URL
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  // Write response body to file
+  fs.writeFile(filePath, body, { encoding: 'utf-8' }, (error) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    console.log(`File saved to ${filePath}`);
+  });
+});
